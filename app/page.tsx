55a63888200f3707713b48a33b1871206f283ed4
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2026-01-05 21:12:26 -0300

    refactor: move Sidebar component from page to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import { Sidebar } from "@/components/Sidebar";
 import { Content } from "@/components/Content";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col md:flex-row">
-      <Sidebar />
       <Content />
     </div>
   );
